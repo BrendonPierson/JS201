64f7ece06bd91aa0f55022a96879f3dbd8db0434
BrendonPierson <brendonpierson@gmail.com>
2015-08-18 15:29:18 -0500

    fixed the text area to array function

--- javascripts/addFamily.js
+++ javascripts/addFamily.js
@@ -1,18 +1,27 @@
 define([], function(){
   return {
     addFamilyMember: function(){
+    var skillsArray = $("#skills").val().split(',');
     var famMember = {
       name: $("#name").val(),
       age: $("#age").val(),
       sex: $('input:checked').val(),
-      skillsArray: $("#skills").val().split(',')
+      skills: skillsArray
     };
+      console.log("raw input skilss", $("#skills").val());
+      console.log("skillsArray", skillsArray);
+      console.log("sex: ", $('input:checked').val());
     $.ajax({
       url: "https://nss-brendon-family.firebaseio.com/family.json",
       method: "POST",
       data: JSON.stringify(famMember)
     }).done(function(){
       console.log("successfully added fammember");
+      $("#name").val('');
+      $("#age").val('');
+      $("#skills").val('');
+      // $("#myModal").slideUp("slow");
+      $('#myModal').modal('hide');
     });
 
 
